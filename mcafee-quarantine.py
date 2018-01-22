afe67acfd7e9c26a5440c3b1fec4ace8847bac0b
Fetchered <10457978+Fetchered@users.noreply.github.com>
2018-01-22 03:06:34 +0000

    Added mcafee-quarantine.py
    Very short python script to assist with the recovery of McAfee .bup files.

---
+++ mcafee-quarantine.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#This little script is meant to assist in the recovery of files from the McAfee Quarantine .bup 'format'.
+#It essentially xor's the contents of the bup file (Details and File_0) with 0x6A
+
+#Provided by Corey Forman (fetchered [at] gmail [dot] com)
+
+import sys
+
+def xor():
+    try:
+        byte=bytearray(open(sys.argv[1], 'rb').read())
+        for i in range(len(byte)):
+	    byte[i] ^= 0x6A
+        open(sys.argv[2], 'wb').write(byte)
+    except:
+        print '\033[1;31m[!] Check your inputs and try again\033[1;31m'
+xor()
